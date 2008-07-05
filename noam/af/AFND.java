c8c883cf6641dbe7a2b3f760cf28fef96b1a895e
migeier <migeier@3f71fe49-3951-0410-86c9-bbeb540de143>
2008-07-05 01:01:07 +0000

    interfaz para AF y agregamos metodos a AFND para que compile con la interfaz nueva

--- noam/af/AFND.java
+++ noam/af/AFND.java
@@ -1,30 +1,73 @@
 package noam.af;
 
 import java.util.HashMap;
-import java.util.Vector;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.Map.Entry;
 
-public class AFND extends AF {
-	
-	// cada nodo tiene asociada una lista de adyacencia en la cual la transicion es la key
-	private HashMap<String, HashMap<String, String> > graph;
+public class AFND implements AF {
+
+	// cada nodo tiene asociada una lista de adyacencia en la cual la transicion
+	// es la key
+	private HashMap<String, HashMap<String, Set<String>>> graph;
+
 	private String initialSt;
-	private Vector<String> finalSts;
+
+	private Set<String> finalSts;
 
 	public AFND() {
+		graph = new HashMap<String, HashMap<String, Set<String>>>();
+		finalSts = new HashSet<String>();
 	}
-	
+
 	public void addNode(String name) {
-		graph.put(name, new HashMap<String, String>());
+		if (!graph.containsKey(name))
+			graph.put(name, new HashMap<String, Set<String>>());
 	}
-	
+
 	public void addTransition(String src, String trans, String dst) {
-		graph.get(src).put(trans, dst);
+		if (!graph.containsKey(src))
+			graph.put(src, new HashMap<String, Set<String>>());
+		if (!graph.get(src).containsKey(trans))
+			graph.get(src).put(trans, new HashSet<String>());
+		graph.get(src).get(trans).add(dst);
 	}
-	
+
 	public void setInitialState(String initial) {
 		initialSt = initial;
 	}
-	
-	
 
+	public Iterator<String> getFinalStates() {
+		return finalSts.iterator();
+	}
+
+	public String getInitialState() {
+		return initialSt;
+	}
+
+	public Iterator<String> getStates() {
+		return graph.keySet().iterator();
+	}
+
+	public Iterator<Transition> getTransitions(String from) {
+		LinkedList<Transition> r = new LinkedList<Transition>();
+		for (Entry<String, Set<String>> entry : graph.get(from).entrySet()) {
+			for (String dest : entry.getValue()) {
+				r.add(new Transition(from, entry.getKey(), dest));
+			}
+		}
+
+		return r.iterator();
+	}
+
+	public Iterator<Transition> getTransitions(String from, String label) {
+		LinkedList<Transition> r = new LinkedList<Transition>();
+		for (String dest : graph.get(from).get(label)) {
+			r.add(new Transition(from, label, dest));
+		}
+
+		return r.iterator();
+	}
 }
