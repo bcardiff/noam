98e8fa26c7226055644319bf38e6aec954344c95
bcardiff <bcardiff@3f71fe49-3951-0410-86c9-bbeb540de143>
2008-07-10 19:41:17 +0000

    Minimization

--- trunk/src/noam/af/algorithms/Minimization.java
+++ trunk/src/noam/af/algorithms/Minimization.java
@@ -1,23 +1,31 @@
 package noam.af.algorithms;
 
-import static noam.utils.IteratorHelper.*;
+import static noam.utils.IteratorHelper.addAll;
+import static noam.utils.IteratorHelper.difference;
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
+import java.util.Set;
+import java.util.TreeMap;
 
 import noam.af.AF;
 import noam.af.Transition;
+import noam.utils.Function;
+import noam.utils.StringHelper;
 
 public class Minimization implements AF {
 
 	AF inner;
 	List<HashSet<String>> statesPartition;
+	TreeMap<String, Integer> statesToPartition;
+	Function<Transition, Transition> innerToThis;
 
+	// requires Complete, Deterministict AF
 	public Minimization(AF inner) {
 		this.inner = inner;
-		
+
 		// initial states partitios {S/F, F}
 		statesPartition = new LinkedList<HashSet<String>>();
 		HashSet<String> innerFinalStates = new HashSet<String>();
@@ -26,49 +34,150 @@ public class Minimization implements AF {
 		addAll(restOfStates, difference(inner.getStates(), innerFinalStates));
 		statesPartition.add(restOfStates);
 		statesPartition.add(innerFinalStates);
-		
+
 		computeStatesPartition();
+		
+		// fill statesToPartition
+		statesToPartition = new TreeMap<String, Integer>();
+		for (int i = 0; i < statesPartition.size(); i++) {
+			statesToPartition.put(StringHelper.asString(i), i);
+		}
+		
+		innerToThis = new Function<Transition, Transition>(){
+			public Transition apply(Transition s) {
+				return new Transition(
+						StringHelper.asString(classOf(s.getFrom())), 
+						s.getLabel(), 
+						StringHelper.asString(classOf(s.getTo())));
+			}			
+		};
 	}
 
 	private void computeStatesPartition() {
+		int countOfStates = statesPartition.size();
+		int prevCountOfStates;
+		do {
+			prevCountOfStates = countOfStates;
+			computeNextStatesPartition();
+			countOfStates = statesPartition.size();
+		} while (countOfStates != prevCountOfStates);
+	}
+
+	private void computeNextStatesPartition() {
 		LinkedList<HashSet<String>> nextPartition = new LinkedList<HashSet<String>>();
-		// TODO
-	}
 
+		for (HashSet<String> eqClass : statesPartition) {
+			while (eqClass.size() > 0) {
+				HashSet<String> newEqClass = new HashSet<String>();
+				final String e = takeOne(eqClass);
+				newEqClass.add(e);
+				moveAllThat(newEqClass, eqClass, new Function<String,Boolean>(){
+					public Boolean apply(String s) {
+						Iterator<String> symbolIt = inner.getAlphabet();
+						while (symbolIt.hasNext()){
+							String symbol = symbolIt.next();
+							if (classAfter(s, symbol) != classAfter(e, symbol))
+								return false;
+						}
+						return true;
+					}
+				});
+				nextPartition.add(newEqClass);
+			}
+		}
 		
+		statesPartition = nextPartition;
+	}
+
+	private int classAfter(String s, String symbol) {
+		Iterator<Transition> it = inner.getTransitions(s, symbol);
+		if (!it.hasNext())
+			throw new RuntimeException("automata not complete");
+		Transition t = it.next();
+		if (it.hasNext())
+			throw new RuntimeException("automata not deterministic");
+		
+		return classOf(t.getTo());
+	}
+	
+	private int classOf(String innerState){
+		int i = 0;
+		for (HashSet<String> eqClass : statesPartition) {
+			if (eqClass.contains(innerState))
+				return i;
+			i++;
+		}
+		
+		throw new RuntimeException("target state not in partition");		
+	}
+
+	private void moveAllThat(HashSet<String> dest,
+			HashSet<String> source, Function<String, Boolean> pred) {
+		Iterator<String> it = source.iterator();
+		while(it.hasNext()){
+			String c = it.next();
+			if (pred.apply(c)){
+				it.remove();
+				dest.add(c);
+			}
+		}
+	}
+
+	private String takeOne(Set<String> s) {
+		Iterator<String> it = s.iterator();
+		String res = it.next();
+		it.remove();
+		return res;
+	}
+
 	public Iterator<String> getFinalStates() {
-		// TODO Auto-generated method stub
-		return null;
+		// we use a set to remove duplicates
+		HashSet<String> res = new HashSet<String>();
+		Iterator<String> innerFinals = inner.getFinalStates();
+		while (innerFinals.hasNext()){
+			res.add(StringHelper.asString(classOf(innerFinals.next())));
+		}
+		return res.iterator();
 	}
 
-	
 	public String getInitialState() {
-		// TODO Auto-generated method stub
-		return null;
+		return StringHelper.asString(classOf(inner.getInitialState()));
 	}
 
-	
 	public Iterator<String> getStates() {
-		// TODO Auto-generated method stub
-		return null;
+		return statesToPartition.keySet().iterator();
 	}
 
-	
 	public Iterator<Transition> getTransitions(String from) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Iterator<Transition>> trans = new LinkedList<Iterator<Transition>>();
+		HashSet<String> eqClass = statesPartition.get(statesToPartition.get(from).intValue());
+		for (String innerState : eqClass) {
+			trans.add(inner.getTransitions(innerState));
+		}
+		return joinTransition(trans);
 	}
 
-	
 	public Iterator<Transition> getTransitions(String from, String label) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Iterator<Transition>> trans = new LinkedList<Iterator<Transition>>();
+		HashSet<String> eqClass = statesPartition.get(statesToPartition.get(from).intValue());
+		for (String innerState : eqClass) {
+			trans.add(inner.getTransitions(innerState, label));
+		}
+		return joinTransition(trans);
 	}
 
-	
+	private Iterator<Transition> joinTransition(List<Iterator<Transition>> trans) {
+		HashSet<Transition> res = new HashSet<Transition>();
+		for (Iterator<Transition> it : trans) {
+			while(it.hasNext()){
+				res.add(innerToThis.apply(it.next()));
+			}
+		}
+		return res.iterator();
+	}
+
 	public Iterator<String> getAlphabet() {
-		// TODO Auto-generated method stub
-		return null;
+		return inner.getAlphabet();
 	}
 
 }
--- trunk/src/noam/utils/StringHelper.java
+++ trunk/src/noam/utils/StringHelper.java
@@ -1,22 +1,46 @@
 package noam.utils;
 
 public class StringHelper {
-	public static Function<String, String> addPrefix(final String prefix){
-		return new Function<String, String>(){
+	public static Function<String, String> addPrefix(final String prefix) {
+		return new Function<String, String>() {
 			public String apply(String s) {
 				return prefix + s;
 			}
 		};
 	}
-	
-	public static Function<String, String> removePrefix(final String prefix){
-		return new Function<String, String>(){
+
+	public static Function<String, String> removePrefix(final String prefix) {
+		return new Function<String, String>() {
 			public String apply(String s) {
 				if (s.startsWith(prefix))
 					return s.substring(prefix.length());
 				else
 					throw new RuntimeException();
-			}			
+			}
 		};
+	}
+
+	/**
+	 * i=0 => A, i=1 => B, ... i=26 => AA, i=27 => AB,...
+	 * @param i
+	 * @return
+	 */
+	public static String asString(int i) {
+		int aCode = 'A';
+		int zCode = 'Z';
+		int lettersCount = zCode - aCode + 1;
+		StringBuffer sb = new StringBuffer();
+		if (i < lettersCount) {
+			sb.append((char) (aCode + i));
+		} else {
+			while (i >= lettersCount) {
+				int l = i % lettersCount;
+				sb.append((char) (aCode + l));
+				i = (i - l) / lettersCount;
+			}
+			sb.append((char) (aCode + i - 1));
+			sb.reverse();
+		}		
+		return sb.toString();
 	}
 }
