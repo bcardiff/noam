130265859d8df2d9bff1b3ead574f5fb8e3d885e
bcardiff <bcardiff@3f71fe49-3951-0410-86c9-bbeb540de143>
2008-07-12 18:46:12 +0000

    AF Minimization

--- trunk/tests/noam/af/algorithms/MinimizationFixture.java
+++ trunk/tests/noam/af/algorithms/MinimizationFixture.java
@@ -14,4 +14,11 @@ public class MinimizationFixture {
 		AF b = new Minimization(a);
 		System.out.append(IO.printDot(b));
 	}
+	
+	@Test
+	public void Hopcroft_48_412() throws Exception {
+		AF a = IO.parseAF("<(A,B,C,D,E,F,G,H),(0,1),((A,0,B)(A,1,F)(B,0,G)(B,1,C)(C,0,A)(C,1,C)(D,0,C)(D,1,G)(E,0,H)(E,1,F)(F,0,C)(F,1,G)(G,0,G)(G,1,E)(H,0,G)(H,1,C)),A,(C)>");
+		AF b = new Minimization(a);
+		System.out.append(IO.printDot(b));
+	}
 }
